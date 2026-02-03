c0bbc5c756c9413beb7f6da98d2a32b015b3820c
Afaq Ahmed Khan <afaqahmed600767@gmail.com>
2026-02-03 14:54:18 +0500

    added policy agent animation

--- components/HrAI.tsx
+++ components/HrAI.tsx
@@ -113,7 +113,7 @@ function RecruitementSection () {
             {gridItems.map((item, idx) => (
               <div
                 key={idx}
-                className={`flex gap-4 sm:gap-6 bg-[#1F7A5A] text-white rounded-lg p-4 text-lg sm:text-xl font-semibold w-full sm:max-w-[350px] z-10
+                className={`flex gap-4 sm:gap-6 bg-[#1F7A5A] text-white rounded-lg p-4 text-lg sm:text-xl font-semibold w-full sm:max-w-[350px] z-10 justify-center
                   ${idx === 6 ? "sm:col-start-2 lg:col-start-2" : ""}
                 `}
               >
@@ -643,6 +643,7 @@ function LeaveSection (){
             width={42}
             height={42}
             style={{ width: "42px", height: "42px", marginTop: 6 }}
+            className="hidden sm:block"
           />}
         </motion.div>
       ))}
@@ -698,6 +699,175 @@ function LeaveSection (){
             ))}
           </div>
         </motion.div>
+      </motion.div>
+        </>
+    )
+}
+
+function PolicySection (){
+    const seeHowRef = useRef(null);
+  const seeHowInView = useInView(seeHowRef, {
+    once: false,
+    amount: 0.1,
+  });
+  const containerRef = useRef(null);
+  const isInView = useInView(containerRef, { once: false, amount: 0.4 });
+  const gridItemsChat = [
+    { img: "/glass.png", text: "Smart Policy Query" },
+    { img: "/retrieval.png", text: "AI Policy Retrieval" },
+    { img: "/book.png", text: "Context-Aware Explanation" },
+    { img: "/tick.png", text: " Instant Employee Resolution" },
+    { img: "/danger.png", text: "Exception Recognition" },
+    { img: "/community.png", text: "Seamless Human Escalation" },
+    { img: "/chart.png", text: "Case Closure Logging" },
+  ];
+  const gridVariants = {
+    hidden: { opacity: 0, height: 0, transition: { duration: 0.3 } },
+    visible: { 
+      opacity: 1, 
+      height: "auto", 
+      transition: { duration: 0.6, ease: "easeInOut" } 
+    }
+  };
+  const cards = [
+    {
+        titlemain:"AI Policy Resolution Agent"
+    },
+    {
+      title: "Policy Request Detected",
+      desc: "System identified inbound query for compliance check.",
+    },
+    {
+      title: "Relevant Policy Identified",
+      desc: "Reference documents retrieved from global database. ",
+    },
+    {
+      title: "Context & Role Evaluated",
+      desc: "User permissions and environmental context verified.",
+    },
+    {
+      title: "Clear Explanation Generated",
+      desc: "Natural language reasoning synthesized for stakeholder.",
+    },
+  ];
+
+  const containerVariants = {
+    hidden: {},
+    visible: {
+      transition: {
+        staggerChildren: 0.2, // cards appear one by one
+      },
+    },
+  };
+
+  const cardVariants = {
+  hidden: (custom: number) => ({
+    opacity: 0,
+    y: 50, // even index from left, odd index from right
+  }),
+  visible: (custom: number) => ({
+    opacity: 1,
+    y: 0,
+    transition: { duration: 0.5, ease: "easeInOut" },
+  }),
+};
+
+    return (
+        <>
+        <motion.div 
+        initial="hidden"
+        variants={gridVariants}
+        animate="visible"   // ✅ REQUIRED
+        exit="hidden"
+        className="flex flex-col gap-4 md:gap-6 mt-20 px-4 md:px-0"
+      >
+        <div className="bg-[#005143] border-white border-2 flex flex-col lg:flex-row gap-4 p-10 md:p-8 mt-10">
+        <div className="text-white flex flex-col gap-7 mt-4 lg:mt-35 ml-0 lg:ml-10">
+          <h2 className="text-2xl font-semibold">Policy Agent</h2>
+          <p className="font-semibold">Instant answers to company policies, explained clearly and applied correctly.</p>
+          <p>
+            The Policy Agent understands employee questions in plain language, retrieves the<br/> right policy, and explains it with real context. When something falls outside the<br/> rules, it flags exceptions and smoothly hands off to HR, with everything logged<br/> automatically.
+          </p>
+          <button className="flex gap-3 sm:gap-5 bg-[#1F7A5A] text-white rounded-lg p-2 sm:p-4 text-lg sm:text-xl w-full sm:w-[200px] font-semibold hover:bg-[#35bd8e] justify-center">
+            <span>Try for Free </span>
+            <MoveRightIcon />
+          </button>
+        </div>
+
+        <motion.div
+      ref={containerRef}
+      className="bg-transparent w-full lg:w-[550px] flex flex-col gap-4 md:gap-9 rounded-md p-4 lg:p-10 lg:ml-auto lg:mr-10 border-14 border-white rounded-lg"
+      variants={containerVariants}
+      initial="hidden"
+      animate={isInView ? "visible" : "hidden"}
+    >
+      {cards.map((card, idx) => (
+        <motion.div
+          key={idx}
+          className="bg-emerald-50 w-full rounded-lg text-white px-6 p-4 flex-col justify-between"
+          variants={idx === 0 ? "visible" : cardVariants}
+          custom={idx}
+        >
+            {card.titlemain && <h2 className="text-[16px] font-semibold text-emerald-800 text-center">{card.titlemain}</h2>}
+            {card.title && <h2 className="text-[16px] font-semibold text-emerald-800">{card.title}</h2>}
+            
+            {card.desc && <p className="text-[13px] text-emerald-700">{card.desc}</p>}
+        </motion.div>
+      ))}
+    </motion.div>
+      </div>
+        <h1 className="text-white text-2xl md:text-3xl text-center mb-4 mt-12" ref={seeHowRef}>
+          See How it works
+        </h1>
+        <motion.div
+  initial={false}
+  animate={seeHowInView ? "open" : "closed"}
+  variants={{
+    closed: {
+      height: 0,
+      opacity: 0,
+      overflow: "hidden",
+    },
+    open: {
+      height: "auto",
+      opacity: 1,
+      transition: {
+        height: { duration: 0.6, ease: "easeInOut" },
+        opacity: { duration: 0.4, delay: 0.2 },
+      },
+    },
+  }}
+  className="relative mt-10 w-full"
+>
+          {/* SVG Lines */}
+          <svg className="absolute w-full h-full pointer-events-none hidden sm:block" viewBox="0 0 1000 1000" preserveAspectRatio="none">
+            <AnimatedPath isInView={seeHowInView} d="
+              M150 150
+              H1000
+              V350
+              H0
+              V540
+              H1000
+              V880
+              H600
+            " />
+          </svg>
+
+          {/* Grid Items */}
+          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 mt-5 items-center justify-items-center gap-4 sm:gap-6 lg:gap-10 px-2 sm:px-4 lg:px-0">
+            {gridItemsChat.map((item, idx) => (
+              <div
+                key={idx}
+                className={`flex gap-4 sm:gap-6 bg-[#1F7A5A] text-white rounded-lg p-4 text-lg sm:text-xl font-semibold w-full sm:max-w-[400px] z-10 justify-center
+                  ${idx === 6 ? "sm:col-start-2 lg:col-start-2" : ""}
+                `}
+              >
+                <Image src={item.img} alt={item.text} width={39} height={30} />
+                <span className="mt-1">{item.text}</span>
+              </div>
+            ))}
+          </div>
+          </motion.div>
       </motion.div>
         </>
     )
@@ -794,10 +964,11 @@ export default function HrAI() {
       {/* Recruitment Agent Section */}
 
       <AnimatePresence mode="wait">
-  {/*{activeSection === 0 && <RecruitementSection key="recruitment" />}*/}
+  {/*activeSection === 0 && <RecruitementSection key="recruitment" />*/}
   {/*activeSection ===0 && <PayrollSection key="payroll" />*/}
   {/*activeSection === 0 && <AttendanceSection key="attendance" />*/}
-  {activeSection === 0 && <LeaveSection key="leave" />}
+  {/*activeSection === 0 && <LeaveSection key="leave" />*/}
+  {/*activeSection === 0 && <PolicySection key="policy" />*/}
 </AnimatePresence>
 
 
